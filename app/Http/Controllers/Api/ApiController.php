0889b7502b4fc2660f11997aa1993e9a1736049e
thoanv <vanthoa225@gmail.com>
2022-04-12 15:42:05 +0700

    Hiển thị anh bìa cho từng trang

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -69,6 +69,7 @@ class ApiController extends Controller
                 "data" =>  []
             ]);
         }
+        $data['category'] =  new CategoryResource($category);
         switch ($category['type']) {
             case 'news':
                 if($category['parent_id']){
